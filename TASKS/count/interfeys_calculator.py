655284f5969cd24532757891f5e6e65f4d386c2f
lev1nzz <xlev1n.xxxx@gmail.com>
2025-06-19 01:05:26 +0300

    обновил readme файл и дописал финал

--- TASKS/count/interfeys_calculator.py
+++ TASKS/count/interfeys_calculator.py
@@ -1,9 +1,10 @@
 from tkinter import *
 from tkinter import messagebox
+from tkinter import ttk
 import math
 
 
-def show_massage():
+def show_massage(): #Функция для выполнения вычислений
     name1 = int(count.get())
     name3 = (cont3.get())
     name2 = int(cont2.get())
@@ -13,12 +14,12 @@ def show_massage():
     summa = name1 + name2
     vichet = name1 - name2
     
-    if name3 == "*":
+    if name3 == "*": #логика вывода ответа
         messagebox.showinfo( "Твой ответ", f"{name1}  *  {name2}  =  {umn}")
-        if name3 == "/" and name1 > name2: #пофиксить с деление с остатком (если число < вылезает 2 окна)
-         messagebox.showinfo( "Твой ответ", f"{name1}  /  {name2}  =  {umn1}")
-        elif name1 < name2:
-            messagebox.showinfo( "Твой ответ", f"{name1}  /  {name2}  =  {umn2}")
+    if name3 == "/" and name1 > name2:
+        messagebox.showinfo( "Твой ответ", f"{name1}  /  {name2}  =  {umn1}")
+    elif name3 == "/" and name1 < name2:
+        messagebox.showinfo( "Твой ответ", f"{name1}  /  {name2}  =  {umn2}")
     if name3 == "+":
         messagebox.showinfo( "Твой ответ", f"{name1}  +  {name2}  =  {summa}")
     if name3 == "-":
@@ -28,11 +29,11 @@ def show_massage():
         
         
 
-root = Tk()
+root = Tk() #Основное окно приложения
 root.title("Мой калькулятор")
-root.geometry("300x310")
-    
-Label(root, text="Введи первое число:").pack(pady=10)
+root.geometry("350x300")
+
+Label(root, text="Введи первое число:").pack(pady=10) #поля для получения числа и операции
 count = Entry(root)
 count.pack(pady=5)
 Label(root, text="Введи операцию:").pack(pady=10)
@@ -43,7 +44,7 @@ cont2 = Entry(root)
 cont2.pack(pady=5)
 
     
-Button(root, text="Ответ", command=show_massage).pack(pady=15)
+Button(root, text="Ответ", command=show_massage).pack(pady=15) #кнопки в интерфейсе
 Button(root, text="Закрыть", command=root.quit).pack(pady=5)
 
 root.mainloop()
